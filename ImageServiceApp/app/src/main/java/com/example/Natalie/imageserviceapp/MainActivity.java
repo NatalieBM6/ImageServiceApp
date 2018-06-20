0acf4a241bc34449bf3ee207fc9389d35b5762b5
NatalieBM6 <33790319+NatalieBM6@users.noreply.github.com>
2018-06-20 04:04:42 +0300

    an update to the class.

--- ImageServiceApp/app/src/main/java/com/example/Natalie/imageserviceapp/MainActivity.java
+++ ImageServiceApp/app/src/main/java/com/example/Natalie/imageserviceapp/MainActivity.java
@@ -3,25 +3,45 @@ package com.example.Natalie.imageserviceapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * the function called when the application is activated.
+     * @param savedInstanceState the instance state save
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * the function called when the start service button is pressed
+     * @param view the current view
+     */
     public void startService(View view) {
+
         Intent intent = new Intent(this, ImageService.class);
-
-        Button start_button = (Button) findViewById(R.id.StartService);
-//        Button stop_button = (Button) findViewById(R.id.stopService_b);
-        start_button.setVisibility(View.VISIBLE);
-//        stop_button.setVisibility(View.VISIBLE);
-
         startService(intent);
+    }
+
+    /**
+     * the function called when the stop service button is pressed
+     * @param view the current view
+     */
+    public void stopService(View view) {
+        Intent intent = new Intent(this, ImageService.class);
+        stopService(intent);
     }
 }
